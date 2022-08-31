acdae13727a6b7ca52ea66e22532a5cc4c46f6f4
Piousangel <yyh7654@gmail.com>
2022-09-01 04:44:01 +0900

    feat : swagger를 사용하여 API 정리

--- src/boss-raid/boss-raid.controller.ts
+++ src/boss-raid/boss-raid.controller.ts
@@ -3,6 +3,7 @@ import { User } from 'src/users/users.entity';
 import { BossRaidEntity } from './boss-raid.entity';
 import { BossRaidService } from './boss-raid.service';
 import moment from 'moment';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('boss-raid')
 @UseInterceptors(CacheInterceptor)
@@ -10,7 +11,7 @@ export class BossRaidController {
 
     constructor(private bossraidService: BossRaidService){}
 
-    // 보스레이드상태 조회 (입장가능한지 혹은 현재 진행중인 유저가 있다면, 해당 유저의 id)
+    @ApiOperation({ summary: '보스레이드상태 조회 (입장가능한지 혹은 현재 진행중인 유저가 있다면, 해당 유저의 id반환'}) 
     @Get('/')
     async getBossRaidStatus() {
         
@@ -19,14 +20,14 @@ export class BossRaidController {
         return {"isEntered" : arr[0], "raidRecordId" : arr[1]}
     }
 
-    // 서비스단에서 조회 가능한지 따져서 가능하면 보스레이드 하기
+    @ApiOperation({ summary: '서비스단에서 조회 가능한지 따져서 가능하면 보스레이드 하기'})
     @Post('/enter')
     async startBossRaid( @Body() body) {
 
         return await this.bossraidService.startBossRaid(body.userId, body.level);
     }
 
-    // 보스레이드 종료하기 레이드 level에 따라 score 반영 (level은 0: 20, 1: 47, 2 : 85)
+    @ApiOperation({ summary: '보스레이드 종료하기 레이드 level에 따라 score 반영 (level은 0: 20, 1: 47, 2 : 85)'}) 
     @Patch('/end')
     async endBossRaid( @Body() body) {
 
@@ -34,7 +35,7 @@ export class BossRaidController {
         return {"isEntered" : result, "comment" : "레이드 종료, 랭킹 업데이트"}
     }
 
-    // 보스레이드 랭킹 조회 응답으로 전체 순위 리스트, id -> ranking
+    @ApiOperation({ summary: '보스레이드 랭킹 조회 응답으로 전체 순위 리스트, id -> ranking'})
     @Post('/topRankerList')
     async searchBossRaidRanking( @Body() body) {
 
@@ -42,11 +43,5 @@ export class BossRaidController {
     }
     
     
-    // @Get()
-    // async getRanking() {
-    //     findAl(): string[] {
-    //         return [];
-    //       }
-    // }
 
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,22 +3,41 @@ import { userInfo } from 'os';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
+import { ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Swagger')
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService){}
 
-    //유저 생성 -> 유저 아이디 반환
+    @ApiOperation({ summary: '유저 생성 -> 유저 아이디 반환'}) // api 설명
     @Post('/')
     @UsePipes(ValidationPipe)  //중복검사
+    @ApiConsumes('multipart/form-data') // Body를 받을 때의 mime type 설정
+    // Body에 대한 명세 설정
+    @ApiBody({
+      description: 'signup',
+      type: 'number',
+    })
+    postSwagger() {
+        return 'this is post swagger paoge';
+    }
     async createUser( @Body() body ) {
         // console.log("body", body)
         const userId = await this.usersService.createUser(body.userId);
         return {"userId" : userId}
     }   
 
-    // 아이디로 유저 조회
+    @ApiOperation({ summary: '아이디로 유저 조회'}) 
+    @ApiResponse({
+        status: 403,
+        description: 'get user id',
+    })
+    @ApiParam({
+        name: 'id param',
+        type: 'number',
+      })
     @Get('/:id')
     async getUserById( @Param('id') userId : number): Promise<User>{
         // console.log(userId)
