cdcccebf8bf325061ac24f074186771cb2587264
Piousangel <yyh7654@gmail.com>
2022-09-01 00:34:58 +0900

    fixed : del config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,17 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `${__dirname}/../env/.$process.env.NODE_ENV}.env`,
+      envFilePath: `${__dirname}/../env/.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot(
     {
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWD,
-      database: process.env.DB_NAME,
+      database: 'testdb',
       synchronize: true, // local only
       entities: [User, BossRaidEntity],
       })
